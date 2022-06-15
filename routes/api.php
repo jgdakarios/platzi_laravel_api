3d71d162a6075d9c03da8805313b5a67a9a23325
Dakarios <jgdakarios@gmail.com>
2022-06-15 21:48:34 +0000

    Versión 1: colección

--- routes/api.php
+++ routes/api.php
@@ -14,4 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::apiResource('v1/posts', App\Http\Controllers\Api\V1\PostController::Class)->only('show');
+Route::apiResource('v1/posts', App\Http\Controllers\Api\V1\PostController::Class)
+    ->only(['index', 'show']);
