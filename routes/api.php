7f37707724541b26301f2916d2b3c68c82a8b002
Dakarios <jgdakarios@gmail.com>
2022-06-15 21:21:07 -0400

    Repaso del código | Delete method.

--- routes/api.php
+++ routes/api.php
@@ -15,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::apiResource('v1/posts', App\Http\Controllers\Api\V1\PostController::Class)
-    ->only(['index', 'show']);
+    ->only(['index', 'show', 'destroy']);
