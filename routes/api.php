be460981c974e68528187f840c98023b7f55f3a8
Dakarios <jgdakarios@gmail.com>
2022-06-15 22:26:44 -0400

    Introducción a la API privada

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,9 @@ use App\Http\Controllers\Api\V1\PostController as PostV1;
 use App\Http\Controllers\Api\V2\PostController as PostV2;
 
 Route::apiResource('v1/posts', PostV1::Class)
-    ->only(['index', 'show', 'destroy']);
+    ->only(['index', 'show', 'destroy'])
+    ->middleware('auth:sanctum');
 
 Route::apiResource('v2/posts', PostV2::Class)
-    ->only(['index', 'show']);
+    ->only(['index', 'show'])
+    ->middleware('auth:sanctum');
