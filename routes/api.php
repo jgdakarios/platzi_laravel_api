b5df02b1fda59208ca4bacfb69a4ecffe6b469ae
Dakarios <jgdakarios@gmail.com>
2022-06-15 23:01:47 -0400

    Autenticación

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\PostController as PostV1;
 use App\Http\Controllers\Api\V2\PostController as PostV2;
 
+Route::post('login', [App\Http\Controllers\Api\LoginController::class, 'login']);
+
 Route::apiResource('v1/posts', PostV1::Class)
     ->only(['index', 'show', 'destroy'])
     ->middleware('auth:sanctum');
